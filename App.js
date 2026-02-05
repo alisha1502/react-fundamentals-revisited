5899b4a67edeafaa3fbac9d733d616cfeeabf3b7
alisha_banka <alishabanka@gmail.com>
2026-02-05 14:40:45 +0530

    Basics: Integrated React into project using cdn links of react and react DOM

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * <div id='grandparent'>
+ *  <div id='parent'>
+ *   <div id='child'>
+ *    <h1>I am h1 tag</h1>
+ *   <h2>I am h2 tag</h2>
+ *  </div>
+ * </div>
+ * </div> 
+ *
+ */
+
+const grandparent = React.createElement("div",{
+    id:'grandparent'
+}, React.createElement("div",{
+    id:'parent'
+}, React.createElement("div",{
+    id:'child'
+}, 
+[React.createElement("h1",null,"I am h1 tag"),
+React.createElement("h2",null,"I am h2 tag")
+]
+)))
+
+
+
+
+const heading = React.createElement('h1', null, 'Hello, React!');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
