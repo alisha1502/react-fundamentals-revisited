5541a24ba9195aa40e9bae1223b4f3852cad533e
alisha_banka <alishabanka@gmail.com>
2026-02-10 20:59:29 +0530

    React element v/s React Functional component and way of calling inside JSX + Component Composition

--- App.js
+++ App.js
@@ -14,17 +14,17 @@ import ReactDOM from 'react-dom/client'
  */
 
 // React.createElement(type, props, children) -> React element (JS Object) -> HTML (DOM element, renders on the screen)
-const grandparent = React.createElement("div",{
-    id:'grandparent'
-}, React.createElement("div",{
-    id:'parent'
-}, React.createElement("div",{
-    id:'child'
-}, 
-[React.createElement("h1",{ key: "h1" },"I am h1 tag"),
-React.createElement("h2",{ key: "h2" },"I am h2 tag")
-]
-)))
+// const grandparent = React.createElement("div",{
+//     id:'grandparent'
+// }, React.createElement("div",{
+//     id:'parent'
+// }, React.createElement("div",{
+//     id:'child'
+// }, 
+// [React.createElement("h1",{ key: "h1" },"I am h1 tag"),
+// React.createElement("h2",{ key: "h2" },"I am h2 tag")
+// ]
+// )))
 
 //JSX ->HTML like syntax which is not HTML
 
@@ -32,5 +32,49 @@ React.createElement("h2",{ key: "h2" },"I am h2 tag")
 const jsxHeading = <h1>Hello, React using JSX!</h1>;
 
 const heading = React.createElement('h1', null, 'Hello, React!');
+
+//React Component
+const Title = () => (
+    <h1>This is a Title Component</h1>
+);
+
+//React functional component - It should be Written as Title (PascalCase)
+const title = () => {
+    return(
+        <div className='title-container'>
+            <h1>This is a Title Component using JSX</h1>
+        </div>
+    )
+}
+
+//React element
+const titleElement = (
+    <div className='title-element-container'>
+        <h1>This is a Title Element using JSX</h1>
+    </div>
+);
+
+
+//Component Composition (a component can be used inside another component)
+// const HeadingComponent = () => {
+//     return (
+//         <div className='container'>
+//             <Title />
+//             <h1>Hello, I am a React Component!</h1>
+//         </div>
+//     );
+// }
+
+const HeadingComponent = () => {
+    return (
+        <div className='container'>
+            {titleElement}
+            <h1>Hello, I am a React Component!</h1>
+        </div>
+    );
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsxHeading);
+
+root.render(<HeadingComponent />);
